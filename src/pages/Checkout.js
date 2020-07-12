84b6eabc693410e94040c2fd2e573c43e6314ddd
Manoj Fernando <manojf@99x.lk>
2020-07-12 12:02:40 +0530

    Update Checkout.js

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -5,7 +5,7 @@ import { Elements } from '@stripe/react-stripe-js';
 import CheckoutForm from "../components/CheckoutForm";
 
 const Checkout = () => {
-    const stripePromise = loadStripe('pk_test_51GyWX6KxUqRqwqPQDVgebtGmlSDpV74fw2xY2zU9dPqjgyAZhglzs7YAUHHZIJmyxlsuGlJX8QgxKvEskIuGPo1J00F0pX4AFN');
+    const stripePromise = loadStripe('<stripe_public_key>');
 
     return (
         <section className="checkout-wrapper">
