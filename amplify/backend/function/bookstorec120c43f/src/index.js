2db9b6994d24a8d9ad3fafcc46a3a61ca98e7f5b
afshaiqbal <afshaiqbal43@gmail.com>
2021-08-26 17:48:30 +0530

    first commit

---
+++ amplify/backend/function/bookstorec120c43f/src/index.js
@@ -0,0 +1,40 @@
+const { CognitoIdentityServiceProvider } = require("aws-sdk");
+const cognitoIdentityServiceProvider = new CognitoIdentityServiceProvider();
+const USER_POOL_ID = "us-east-1_UB3oKV7rP";
+const stripe = require("stripe")("sk_test_51JSgfBSGO6rjnl8kR0k06qldW52uUSN1H4wnB93SYFDPpqwgzM1sUWUGwbnRfdAjKu2cG3ldpvSRg2M6hDJY8kpb00C9ryn4hj");
+
+const getUserEmail = async (event) => {
+  const params = {
+    UserPoolId: USER_POOL_ID,
+    Username: event.identity.claims.username
+  };
+  const user = await cognitoIdentityServiceProvider.adminGetUser(params).promise();
+  const { Value: email } = user.UserAttributes.find((attr) => {
+    if (attr.Name === "email") {
+      return attr.Value;
+    }
+  });
+  return email;
+};
+
+/*
+ * Get the total price of the order
+ * Charge the customer
+ */
+exports.handler = async (event) => {
+  try {
+    const { id, cart, total, address, token } = event.arguments.input;
+    const { username } = event.identity.claims;
+    const email = await getUserEmail(event);
+
+    await stripe.charges.create({
+      amount: total * 100,
+      currency: "usd",
+      source: token,
+      description: `Order ${new Date()} by ${username} with ${email} email`
+    });
+    return { id, cart, total, address, username, email };
+  } catch (err) {
+    throw new Error(err);
+  }
+};
