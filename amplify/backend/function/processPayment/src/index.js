08655331380720f17229c08df1aec154a8973953
Bianca Fu <0509biancafu@gmail.com>
2024-01-05 23:32:38 -0700

    initialize auth and bucket

---
+++ amplify/backend/function/processPayment/src/index.js
@@ -0,0 +1,17 @@
+
+
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+exports.handler = async (event) => {
+    console.log(`EVENT: ${JSON.stringify(event)}`);
+    return {
+        statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*",
+    //      "Access-Control-Allow-Headers": "*"
+    //  },
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+};
